7a787dea57b9ccd07c72d09f08732a0a78f61de2
Cologne69 <kleanosiauw@student.ciputra.ac.id>
2021-12-10 12:45:15 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace W13__Jumat_Latihan
+{
+    class Swords
+    {
+       public string Name;
+       public string subStat;
+       public int subSTatValue;
+       public bool isFiveStar;
+       public int baseAtk;
+        
+    }
+    class Polearms
+    {
+        public string Name;
+        public string subStat;
+        public int subSTatValue;
+        public bool isFiveStar;
+        public int baseAtk;
+    }
+    class Claymores
+    {
+        public string Name;
+        public string subStat;
+        public int subSTatValue;
+        public bool isFiveStar;
+        public int baseAtk;
+    }
+    class Catalysts
+    {
+        public string Name;
+        public string subStat;
+        public int subSTatValue;
+        public bool isFiveStar;
+        public int baseAtk;
+    }
+    class Bows
+    {
+        public string Name;
+        public string subStat;
+        public int subSTatValue;
+        public bool isFiveStar;
+        public int baseAtk;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Swords swords = new Swords();
+            Polearms polearms = new Polearms();
+            Claymores claymore = new Claymores();
+            Catalysts catalysts = new Catalysts();
+            Bows bows = new Bows();
+        }
+    }
+}
